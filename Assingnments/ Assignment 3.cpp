6d82fdfcfe36134a7d5cfcf69f69bf4030c38786
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-16 23:53:57 +0600

    Create  Assignment 3.cpp

---
+++ Assingnments/ Assignment 3.cpp
@@ -0,0 +1,94 @@
+/*
+    Rezwan_4029
+    AUST , CSE-25
+*/
+#include <windows.h>
+#include <glut.h>
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL  ({ll  a; scanf("%lld", &a); a;})
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef vector<ll>vl;
+typedef pair<int,int>pii;
+typedef pair<ll,ll>pll;
+typedef pair<double,double>pdd;
+
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+int N ;
+double add;
+enum {BLACK=0,RED,GREEN,BLUE,YELLOW,PURPLE,CYAN,WHITE};
+void __getColor(int s)  {
+    if( s == BLACK )        glColor3f(0,0,0);
+    else if( s == BLUE )    glColor3f(0,0,1);
+    else if( s == GREEN )   glColor3f(0,1,0);
+    else if( s == CYAN )    glColor3f(0,1,1);
+    else if( s == RED )     glColor3f(1,0,0);
+    else if( s == PURPLE )  glColor3f(1,0,1);
+    else if( s == YELLOW )  glColor3f(1,1,0);
+    else if( s == WHITE )   glColor3f(1,1,1);
+}
+
+
+double angle = 0.0;
+
+void Draw() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	__getColor(GREEN);
+	glPointSize(8);
+
+	glPushMatrix();
+	glRotatef(angle, 0.0, 3.0, 2.0);
+		glBegin(GL_QUADS);
+			glVertex3f(0.2, 0.2, 0.0);
+			glVertex3f(0.8, 0.2, 0.0);
+			glVertex3f(0.6, 0.4, 0.0);
+			glVertex3f(0.4, 0.4, 0.0);
+		glEnd();
+	glPopMatrix();
+
+	glFlush();
+}
+
+void update(int value)
+{
+	angle += 0.2;
+	if (angle > 360) angle  =  0.0;
+	glutPostRedisplay();
+	glutTimerFunc(5,update,0);
+}
+
+void Initialize() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+int main(int iArgc, char** cppArgv) {
+	glutInit(&iArgc, cppArgv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow("ROTATE");
+	Initialize();
+	glutDisplayFunc(Draw);
+	glutTimerFunc(0,update,0);
+	glutMainLoop();
+	return 0;
+}
