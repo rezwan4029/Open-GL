7473f2908aa81c104f88aecdd2c9bb4948cec027
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2014-01-27 01:48:58 +0600

    commit

---
+++ Assingnment2/3D Objects/glutWireCone.cpp
@@ -0,0 +1,108 @@
+/*
+    Rezwan_4029
+    AUST , CSE-25
+*/
+#include <windows.h>
+#include <glut.h>
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL  ({ll  a; scanf("%lld", &a); a;})
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef vector<ll>vl;
+typedef pair<int,int>pii;
+typedef pair<ll,ll>pll;
+typedef pair<double,double>pdd;
+
+#define forab(i, a, b)        for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)                forab (i, 0, (n) - 1)
+#define For(i, n)                forab (i, 1, n)
+#define rofba(i, a, b)        for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)                rofba (i, 0, (n) - 1)
+#define rof(i, n)                rofba (i, 1, n)
+#define forstl(i, s)        for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+
+
+enum {BLACK=0,RED,GREEN,BLUE,YELLOW,PURPLE,CYAN,WHITE};
+void getColor(int s)  {
+    if( s == BLACK )        glColor3f(0,0,0);
+    else if( s == BLUE )    glColor3f(0,0,1);
+    else if( s == GREEN )   glColor3f(0,1,0);
+    else if( s == CYAN )    glColor3f(0,1,1);
+    else if( s == RED )     glColor3f(1,0,0);
+    else if( s == PURPLE )  glColor3f(1,0,1);
+    else if( s == YELLOW )  glColor3f(1,1,0);
+    else if( s == WHITE )   glColor3f(1,1,1);
+}
+
+int slices = 20 ;
+int slacks = 20 ;
+double ang = 0 ;
+
+void Draw() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  ang += .03;
+  getColor(BLUE);
+  glPushMatrix();
+        glTranslated(0,0.5,-4);
+        glRotated(60,1,0,0);
+        glRotated(ang,0,0,1);
+        glutWireCone(1,1,slices,slacks);
+  glPopMatrix();
+  glFlush();
+  glutSwapBuffers(); // just keep
+}
+
+
+void windowKey(unsigned char key,int x,int y) {
+  if (key == '+' )  slacks++ , slices++;
+  else if (key == '-')slacks-- , slices--;
+  glutPostRedisplay();
+}
+
+void idle()
+{
+    glutPostRedisplay();
+}
+
+void resize(int width, int height)
+{
+    const double ar = (double) width / (double) height;
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
+
+void Initialize() {
+	glClearColor(1, 1, 1, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
+}
+int main(int argc,char* argv[]) {
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(500,500);
+  glutInitWindowPosition(30, 30);
+  glutCreateWindow("GlutWireCone");
+  Initialize();
+  glutReshapeFunc(resize);
+  glutDisplayFunc(Draw);
+  glutKeyboardFunc(windowKey);
+  glutIdleFunc(idle);
+  glutMainLoop();
+  return 0;
+}
